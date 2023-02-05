c96ca00220ab44aa74792b85ce6c05cb507c202b
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2023-02-04 17:50:53 -0800

    WIP game piece tracker.

---
+++ Cthulhu_2023/src/main/java/frc/robot/subsystems/GamePieceTracker.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+
+import org.opencv.core.Rect2d;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.TargetCorner;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.BreakerLib.devices.vision.photon.BreakerFiducialPhotonTarget;
+import frc.robot.BreakerLib.devices.vision.photon.BreakerPhotonCamera;
+import frc.robot.BreakerLib.devices.vision.photon.BreakerPhotonTarget2d;
+
+enum GamePieceType {
+  CONE_UPRIGHT,
+  CONE_TOPPLED,
+  CUBE,
+  NONE
+}
+
+public class GamePieceTracker extends SubsystemBase {
+  private BreakerPhotonCamera coneCam, cubeCam;
+  private PhotonTrackedTarget bestCone, bestCube;
+  private double datatTimestamp;
+  private GamePieceType currentPieceType;
+
+  
+  /** Creates a new GamePieceVision. */
+  public GamePieceTracker() {
+    coneCam = new BreakerPhotonCamera(DUMMY, DUMMY);
+    cubeCam = new BreakerPhotonCamera(DUMMY, DUMMY);
+  }
+
+  public Rect2d makeBoundingBox(PhotonTrackedTarget target) {
+    var corners = target.getDetectedCorners();
+    TargetCorner firstCorner = corners.remove(0);
+  }
+
+  @Override
+  public void periodic() {
+    if (coneCam.hasTargets()) {
+      // Assuming cones are placed into rectangular bounding boxes
+      bestCone = coneCam.getBestTarget();
+    }
+    else if (cubeCam.hasTargets()) {
+      bestCube = cubeCam.getBestTarget();
+      currentPieceType = GamePieceType.CUBE;
+    }
+  }
+}
