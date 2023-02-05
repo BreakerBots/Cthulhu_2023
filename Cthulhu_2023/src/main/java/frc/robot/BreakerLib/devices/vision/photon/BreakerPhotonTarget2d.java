0bddba6579290206fef42a2f34ca662621c6c020
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2023-02-04 17:50:31 -0800

    Remove unused imports

--- Cthulhu_2023/src/main/java/frc/robot/BreakerLib/devices/vision/photon/BreakerPhotonTarget2d.java
+++ Cthulhu_2023/src/main/java/frc/robot/BreakerLib/devices/vision/photon/BreakerPhotonTarget2d.java
@@ -16,8 +16,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.BreakerLib.position.odometry.BreakerGenericOdometer;
 import frc.robot.BreakerLib.util.math.BreakerUnits;
