4bafd231cc6d17ff684ccb70f6c5cac7f886586a
Roman-Tech-Plus <rrcabrahamson@gmail.com>
2023-02-04 23:19:38 -0800

    Update GamePieceTracker.java

--- Cthulhu_2023/src/main/java/frc/robot/subsystems/GamePieceTracker.java
+++ Cthulhu_2023/src/main/java/frc/robot/subsystems/GamePieceTracker.java
@@ -5,12 +5,14 @@
 package frc.robot.subsystems;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.opencv.core.Rect2d;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
 
 import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.BreakerLib.devices.vision.photon.BreakerFiducialPhotonTarget;
 import frc.robot.BreakerLib.devices.vision.photon.BreakerPhotonCamera;
@@ -25,36 +27,112 @@ enum GamePieceType {
 
 public class GamePieceTracker extends SubsystemBase {
   private BreakerPhotonCamera coneCam, cubeCam;
-  private PhotonTrackedTarget bestCone, bestCube;
-  private double datatTimestamp;
-  private GamePieceType currentPieceType;
+  private double uprightConeBoundRatioThreshold = 1.2;
+  private ArrayList<TrackedGamePiece> trackedGamePieces;
 
   
   /** Creates a new GamePieceVision. */
   public GamePieceTracker() {
-    coneCam = new BreakerPhotonCamera(DUMMY, DUMMY);
-    cubeCam = new BreakerPhotonCamera(DUMMY, DUMMY);
+    coneCam = new BreakerPhotonCamera("CONE_CAM_1", new Transform3d());
+    cubeCam = new BreakerPhotonCamera("CUBE_CAM_1", new Transform3d());
+    trackedGamePieces = new ArrayList<>();
   }
 
-  public Pair<Double, Double> makeBoundingBox(PhotonTrackedTarget target) {
-    var corners = target.getDetectedCorners();
+  private boolean isConeUpright(VisionBoundingBox coneBound) {
+    return coneBound.getHight() / coneBound.getWidth() >= uprightConeBoundRatioThreshold;
+  } 
+
+  private VisionBoundingBox makeBoundingBox(PhotonTrackedTarget target) {
+    var corners = target.getMinAreaRectCorners();
     TargetCorner firstCorner = corners.remove(0);
+    TargetCorner opposingCorner = firstCorner;
     for (TargetCorner tc: corners) {
       if (tc.x != firstCorner.x && tc.y != firstCorner.y) {
-        return new Pair();
+        opposingCorner = tc;
+      }
+    }
+    return new VisionBoundingBox(Math.abs(opposingCorner.x - firstCorner.x), Math.abs(opposingCorner.y - firstCorner.y));
+  }
+
+  public boolean hasTargets() {
+    return !trackedGamePieces.isEmpty();
+  }
+
+  public TrackedGamePiece getBestTrackedGamePiece() {
+    return trackedGamePieces.get(0);
+  }
+
+  public ArrayList<TrackedGamePiece> getTrackedGamePieces() {
+    return new ArrayList<>(trackedGamePieces);
+  }
+
+ 
+
+  @Override
+  public void periodic() {
+    trackedGamePieces.clear();
+    if (coneCam.hasTargets()) {
+      for (PhotonTrackedTarget target: coneCam.getAllRawTrackedTargets()) {
+        trackedGamePieces.add(new TrackedGamePiece(isConeUpright(makeBoundingBox(target)) ?  GamePieceType.CONE_UPRIGHT : GamePieceType.CONE_TOPPLED, target));
+      }
+    }
+    if (cubeCam.hasTargets()) {
+      for (PhotonTrackedTarget target: cubeCam.getAllRawTrackedTargets()) {
+        trackedGamePieces.add(new TrackedGamePiece(GamePieceType.CUBE, target));
+      }
+    }
+    if (!trackedGamePieces.isEmpty()) {
+      Collections.sort(trackedGamePieces);
+    }
+  }
+
+  public static class TrackedGamePiece implements Comparable<TrackedGamePiece> {
+    private final double CAMERA_FOV_PITCH = 0;
+    private GamePieceType type;
+    private PhotonTrackedTarget target;
+    public TrackedGamePiece(GamePieceType type, PhotonTrackedTarget target) {
+      this.target = target;
+      this.type = type;
+    }
+
+    public PhotonTrackedTarget getTarget() {
+        return target;
+    }
+
+    public GamePieceType getType() {
+        return type;
+    }
+
+    @Override
+    public int compareTo(TrackedGamePiece arg0) {
+
+      double otherDist = Math.hypot(arg0.target.getYaw(), Math.abs(arg0.target.getPitch() + (CAMERA_FOV_PITCH/2)));
+      double dist = Math.hypot(target.getYaw(), Math.abs(target.getPitch() + (CAMERA_FOV_PITCH/2)));
+      if (dist < otherDist) {
+        return 1;
+      } else if (dist > otherDist) {
+        return -1;
+      } else {
+        return 0;
       }
     }
   }
 
-  @Override
-  public void periodic() {
-    if (coneCam.hasTargets()) {
-      // Assuming cones are placed into rectangular bounding boxes
-      bestCone = coneCam.getBestTarget();
+  public static class VisionBoundingBox {
+    private double width, hight;
+    public VisionBoundingBox(double width, double hight) {
+      this.width = width;
+      this.hight = hight;
     }
-    else if (cubeCam.hasTargets()) {
-      bestCube = cubeCam.getBestTarget();
-      currentPieceType = GamePieceType.CUBE;
+
+
+    public double getHight() {
+        return hight;
     }
-  }
+
+    public double getWidth() {
+        return width;
+    }
+
+  } 
 }
