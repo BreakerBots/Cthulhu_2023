67758de2240cbafbb299d1677265a2d1a35fedd8
Roman-Tech-Plus <rrcabrahamson@gmail.com>
2023-04-01 22:18:15 -0700

    Update BreakerRamsetePathFollower.java

--- Cthulhu_2023/src/main/java/frc/robot/BreakerLib/auto/pathplanner/BreakerRamsetePathFollower.java
+++ Cthulhu_2023/src/main/java/frc/robot/BreakerLib/auto/pathplanner/BreakerRamsetePathFollower.java
@@ -1,34 +1,295 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.BreakerLib.auto.pathplanner;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.server.PathPlannerServer;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.BreakerLib.position.odometry.BreakerGenericOdometer;
+import frc.robot.BreakerLib.subsystem.cores.drivetrain.differential.BreakerDiffDrive;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
+/** Custom PathPlanner version of RamseteCommand */
 public class BreakerRamsetePathFollower extends CommandBase {
-  /** Creates a new BreakerRamsetePathFollower. */
-  public BreakerRamsetePathFollower() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private final Timer timer = new Timer();
+  private final PathPlannerTrajectory trajectory;
+  private final BreakerRamsetePathFollowerConfig config;
+  private final boolean stopAtEnd;
+
+  private DifferentialDriveWheelSpeeds prevSpeeds;
+  private double prevTime;
+
+  private PathPlannerTrajectory transformedTrajectory;
+
+  private static Consumer<PathPlannerTrajectory> logActiveTrajectory = null;
+  private static Consumer<Pose2d> logTargetPose = null;
+  private static Consumer<ChassisSpeeds> logSetpoint = null;
+  private static BiConsumer<Translation2d, Rotation2d> logError = BreakerRamsetePathFollower::defaultLogError;
+
+  /**
+   * Constructs a new PPRamseteCommand that, when executed, will follow the provided trajectory. PID
+   * control and feedforward are handled internally, and outputs are scaled -12 to 12 representing
+   * units of volts.
+   *
+   * <p>Note: The controller will *not* set the outputVolts to zero upon completion of the path -
+   * this is left to the user, since it is not appropriate for paths with nonstationary endstates.
+   *
+   * @param trajectory The trajectory to follow.
+   * @param poseSupplier A function that supplies the robot pose - use one of the odometry classes
+   *     to provide this.
+   * @param controller The RAMSETE controller used to follow the trajectory.
+   * @param feedforward The feedforward to use for the drive.
+   * @param kinematics The kinematics for the robot drivetrain.
+   * @param speedsSupplier A function that supplies the speeds of the left and right sides of the
+   *     robot drive.
+   * @param leftController The PIDController for the left side of the robot drive.
+   * @param rightController The PIDController for the right side of the robot drive.
+   * @param outputVolts A function that consumes the computed left and right outputs (in volts) for
+   *     the robot drive.
+   * @param useAllianceColor Should the path states be automatically transformed based on alliance
+   *     color? In order for this to work properly, you MUST create your path on the blue side of
+   *     the field.
+   * @param requirements The subsystems to require.
+   */
+  public BreakerRamsetePathFollower(
+      BreakerRamsetePathFollowerConfig config,
+      PathPlannerTrajectory trajectory,
+      boolean stopAtEnd
+    ) {
+    this.trajectory = trajectory;
+    this.config = config;
+    this.stopAtEnd = stopAtEnd;
+    addRequirements(config.getDrivetrain());
+
+    if (config.getUseAllianceColor() && trajectory.fromGUI && trajectory.getInitialPose().getX() > 8.27) {
+      DriverStation.reportWarning(
+          "You have constructed a path following command that will automatically transform path states depending"
+              + " on the alliance color, however, it appears this path was created on the red side of the field"
+              + " instead of the blue side. This is likely an error.",
+          false);
+    }
   }
 
-  // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    if (this.config.getUseAllianceColor() && trajectory.fromGUI) {
+      transformedTrajectory =
+          PathPlannerTrajectory.transformTrajectoryForAlliance(
+              trajectory, DriverStation.getAlliance());
+    } else {
+      transformedTrajectory = trajectory;
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
+    this.prevTime = -1;
+
+    if (logActiveTrajectory != null) {
+      logActiveTrajectory.accept(transformedTrajectory);
+    }
+
+    PathPlannerTrajectory.PathPlannerState initialState = transformedTrajectory.getInitialState();
+
+    this.prevSpeeds =
+        this.config.getDrivetrain().getKinematics().toWheelSpeeds(
+            new ChassisSpeeds(
+                initialState.velocityMetersPerSecond,
+                0,
+                initialState.curvatureRadPerMeter * initialState.velocityMetersPerSecond));
+
+    this.timer.reset();
+    this.timer.start();
+
+    this.config.getLeftController().reset();
+    this.config.getRightController().reset();
+
+    PathPlannerServer.sendActivePath(transformedTrajectory.getStates());
+  }
+
   @Override
-  public void execute() {}
+  public void execute() {
+    double currentTime = this.timer.get();
+    double dt = currentTime - this.prevTime;
 
-  // Called once the command ends or is interrupted.
+    if (this.prevTime < 0) {
+      this.prevTime = currentTime;
+      return;
+    }
+
+    Pose2d currentPose = this.config.odometer.getOdometryPoseMeters();
+    PathPlannerTrajectory.PathPlannerState desiredState =
+        (PathPlannerTrajectory.PathPlannerState) transformedTrajectory.sample(currentTime);
+
+    PathPlannerServer.sendPathFollowingData(desiredState.poseMeters, currentPose);
+
+    ChassisSpeeds targetChassisSpeeds = this.config.getRamseteController().calculate(currentPose, desiredState);
+    DifferentialDriveWheelSpeeds targetWheelSpeeds =
+        this.config.getDrivetrain().getKinematics().toWheelSpeeds(targetChassisSpeeds);
+
+    double leftSpeedSetpoint = targetWheelSpeeds.leftMetersPerSecond;
+    double rightSpeedSetpoint = targetWheelSpeeds.rightMetersPerSecond;
+
+    double leftOutput;
+    double rightOutput;
+
+      double leftFeedforward =
+          this.config.getFeedforward().calculate(
+              leftSpeedSetpoint, (leftSpeedSetpoint - this.prevSpeeds.leftMetersPerSecond) / dt);
+      double rightFeedforward =
+          this.config.getFeedforward().calculate(
+              rightSpeedSetpoint, (rightSpeedSetpoint - this.prevSpeeds.rightMetersPerSecond) / dt);
+
+      leftOutput =
+          leftFeedforward
+              + this.config.getLeftController().calculate(
+                  this.config.getDrivetrain().getWheelSpeeds().leftMetersPerSecond, leftSpeedSetpoint);
+      rightOutput =
+          rightFeedforward
+              + this.config.getRightController().calculate(
+                this.config.getDrivetrain().getWheelSpeeds().rightMetersPerSecond, rightSpeedSetpoint);
+
+    this.config.getDrivetrain().tankDriveVoltage(leftOutput, rightOutput);
+    this.prevSpeeds = targetWheelSpeeds;
+    this.prevTime = currentTime;
+
+    if (logTargetPose != null) {
+      logTargetPose.accept(desiredState.poseMeters);
+    }
+
+    if (logError != null) {
+      logError.accept(
+          currentPose.getTranslation().minus(desiredState.poseMeters.getTranslation()),
+          currentPose.getRotation().minus(desiredState.poseMeters.getRotation()));
+    }
+
+    if (logSetpoint != null) {
+      logSetpoint.accept(targetChassisSpeeds);
+    }
+  }
+
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    this.timer.stop();
 
-  // Returns true when the command should end.
+    if (interrupted
+        || Math.abs(transformedTrajectory.getEndState().velocityMetersPerSecond) < 0.1 || stopAtEnd) {
+      this.config.getDrivetrain().stop();
+    }
+  }
+
   @Override
   public boolean isFinished() {
-    return false;
+    return this.timer.hasElapsed(transformedTrajectory.getTotalTimeSeconds());
   }
+
+  private static void defaultLogError(Translation2d translationError, Rotation2d rotationError) {
+    SmartDashboard.putNumber("PPRamseteCommand/xErrorMeters", translationError.getX());
+    SmartDashboard.putNumber("PPRamseteCommand/yErrorMeters", translationError.getY());
+    SmartDashboard.putNumber("PPRamseteCommand/rotationErrorDegrees", rotationError.getDegrees());
+  }
+
+  /**
+   * Set custom logging callbacks for this command to use instead of the default configuration of
+   * pushing values to SmartDashboard
+   *
+   * @param logActiveTrajectory Consumer that accepts a PathPlannerTrajectory representing the
+   *     active path. This will be called whenever a PPRamseteCommand starts
+   * @param logTargetPose Consumer that accepts a Pose2d representing the target pose while path
+   *     following
+   * @param logSetpoint Consumer that accepts a ChassisSpeeds object representing the setpoint
+   *     speeds
+   * @param logError BiConsumer that accepts a Translation2d and Rotation2d representing the error
+   *     while path following
+   */
+  public static void setLoggingCallbacks(
+      Consumer<PathPlannerTrajectory> logActiveTrajectory,
+      Consumer<Pose2d> logTargetPose,
+      Consumer<ChassisSpeeds> logSetpoint,
+      BiConsumer<Translation2d, Rotation2d> logError) {
+    BreakerRamsetePathFollower.logActiveTrajectory = logActiveTrajectory;
+    BreakerRamsetePathFollower.logTargetPose = logTargetPose;
+    BreakerRamsetePathFollower.logSetpoint = logSetpoint;
+    BreakerRamsetePathFollower.logError = logError;
+  }
+
+public static class BreakerRamsetePathFollowerConfig {
+  private BreakerDiffDrive drivetrain;
+  private BreakerGenericOdometer odometer;
+  private RamseteController ramseteController;
+  private SimpleMotorFeedforward feedforward;
+  private PIDController leftController;
+  private PIDController rightController;
+  private boolean useAllianceColor;
+  public BreakerRamsetePathFollowerConfig(
+    BreakerDiffDrive drivetrain, 
+    RamseteController ramseteController, 
+    SimpleMotorFeedforward feedforward,  
+    PIDController leftController,
+    PIDController rightController,
+    boolean useAllianceColor
+    ) {
+      this.ramseteController = ramseteController;
+      this.drivetrain = drivetrain;
+      this.feedforward = feedforward;
+      this.leftController = leftController;
+      this.rightController = rightController;
+      this.useAllianceColor = useAllianceColor;
+      odometer = drivetrain;
+    }
+
+    public BreakerRamsetePathFollowerConfig(
+    BreakerDiffDrive drivetrain, 
+    BreakerGenericOdometer odometer,
+    RamseteController ramseteController, 
+    SimpleMotorFeedforward feedforward,  
+    PIDController leftController,
+    PIDController rightController,
+    boolean useAllianceColor
+    ) {
+      this.ramseteController = ramseteController;
+      this.drivetrain = drivetrain;
+      this.odometer = odometer;
+      this.feedforward = feedforward;
+      this.leftController = leftController;
+      this.rightController = rightController;
+      this.useAllianceColor = useAllianceColor;
+    }
+
+    public BreakerDiffDrive getDrivetrain() {
+        return drivetrain;
+    }
+
+    public SimpleMotorFeedforward getFeedforward() {
+        return feedforward;
+    }
+
+    public PIDController getLeftController() {
+        return leftController;
+    }
+
+    public BreakerGenericOdometer getOdometer() {
+        return odometer;
+    }
+
+    public RamseteController getRamseteController() {
+        return ramseteController;
+    }
+
+    public PIDController getRightController() {
+        return rightController;
+    }
+
+    public boolean getUseAllianceColor() {
+        return useAllianceColor;
+    }
+}
 }
