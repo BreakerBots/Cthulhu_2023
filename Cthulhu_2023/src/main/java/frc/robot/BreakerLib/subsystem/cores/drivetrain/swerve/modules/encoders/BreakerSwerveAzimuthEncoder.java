945501457b112a56d48750dda85eda2fc8a207ff
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2023-05-30 10:50:50 -0700

    Update BreakerSwerveAzimuthEncoder.java

--- Cthulhu_2023/src/main/java/frc/robot/BreakerLib/subsystem/cores/drivetrain/swerve/modules/encoders/BreakerSwerveAzimuthEncoder.java
+++ Cthulhu_2023/src/main/java/frc/robot/BreakerLib/subsystem/cores/drivetrain/swerve/modules/encoders/BreakerSwerveAzimuthEncoder.java
@@ -7,13 +7,23 @@ package frc.robot.BreakerLib.subsystem.cores.drivetrain.swerve.modules.encoders;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.BreakerLib.util.test.selftest.DeviceHealth;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
+// Preferably this becomes a general duty cycle encoder interface.
 
 /** Add your docs here. */
 public interface BreakerSwerveAzimuthEncoder {
+
+    // Unused, should be removed. More fitting for a quad encoder.
     public abstract double getRelative();
+
     public abstract double getAbsolute();
+
     public abstract void config(boolean clockwisePosative, double absoluteOffset);
+
     public abstract Pair<DeviceHealth, String> getFaultData();
+
     public abstract Class<?> getBaseEncoderType();
+
     public abstract Object getBaseEncoder();
 }
